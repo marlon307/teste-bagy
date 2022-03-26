9a0e3c5470c8fc78e7c2d40f65be69ca026e750a
marlon307 <marlon-ramosb@hotmail.com>
2022-03-26 12:50:30 -0300

    Flex tables

--- components/Main/MainFooter/index.js
+++ components/Main/MainFooter/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import BoxProducts from './BoxProducts';
 import BoxTotalBuy from './BoxTotalBuy';
+import style from './style.module.scss';
 
 function FooterMain() {
   return (
-    <section>
+    <section className={ style.footermain }>
       <BoxTotalBuy />
       <BoxProducts />
     </section>
