ed10c88cc567485cc76873dd07440af7d2f63d68
marlon307 <marlon-ramosb@hotmail.com>
2022-03-26 10:17:31 -0300

    Add componentes Footer Main

---
+++ components/Main/MainFooter/BoxPoducts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BoxPoducts() {
+  return (
+    <div>BoxPoducts</div>
+  );
+}
+
+export default BoxPoducts;
---
+++ components/Main/MainFooter/BoxTotalBuy.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './style.module.scss';
+
+function BoxTotalBuy() {
+  return (
+    <div className={ style.boxtotalbuy }>
+      <div style={ style.headerbox }>
+        <div className={ style.primaryLine }>
+          <span>Total Compras</span>
+          <span>List</span>
+        </div>
+        <div className={ style.secondaryLine }>sd</div>
+      </div>
+    </div>
+  );
+}
+
+export default BoxTotalBuy;
