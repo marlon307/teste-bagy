ec39a3967e62c9f69c75616f6eaaabcb4b34890c
marlon307 <marlon-ramosb@hotmail.com>
2022-03-26 10:51:20 -0300

    Style Box total buy

--- components/Main/MainFooter/BoxTotalBuy.js
+++ components/Main/MainFooter/BoxTotalBuy.js
@@ -4,13 +4,39 @@ import style from './style.module.scss';
 function BoxTotalBuy() {
   return (
     <div className={ style.boxtotalbuy }>
-      <div style={ style.headerbox }>
+      <div className={ style.headerbox }>
         <div className={ style.primaryLine }>
-          <span>Total Compras</span>
-          <span>List</span>
+          <h2>Total Compras</h2>
+          <span>Semana</span>
         </div>
-        <div className={ style.secondaryLine }>sd</div>
+        <div className={ style.secondaryLine }>
+          <span>Valor geral: 13.250,00</span>
+        </div>
       </div>
+      <main className={ style.mainbox }>
+        <ul>
+          <li>
+            <span>Estilo Pri</span>
+            <span>250 compras</span>
+            <span>R$ 4238,00</span>
+          </li>
+          <li>
+            <span>Vilma Calçados</span>
+            <span>187 compras</span>
+            <span>R$ 1005,00</span>
+          </li>
+          <li>
+            <span>Mary Lingerie</span>
+            <span>124 compras</span>
+            <span>R$ 914,00</span>
+          </li>
+          <li>
+            <span>Loja Belíssima</span>
+            <span>89 compras</span>
+            <span>R$ 281,00</span>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
